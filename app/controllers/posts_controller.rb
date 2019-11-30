c917e5d37f401e1a7f07ff898b5ab632852db2c7
weyus <wes@redflag.com>
2019-11-30 16:21:04 -0600

    Post creation, comment link, deletion. Initial styling. Minor spelling correction (codeing)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,44 @@
+#Notes:
+# Not worrying about alternate rendering targets other than HTML
+
 class PostsController < ApplicationController
+  before_action :get_post, only: [:show, :destroy]
+
   def index
-    # Return all `Post`
+    @posts = Post.all
   end
 
   def new
-    # Return view to create a new Post
+    @post = Post.new
   end
 
   def create
-    # Add a new `Post` to the database
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  def show
   end
 
   def destroy
-    # Remove a `Post` from the database
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  #Provide post for actions that need it
+  def get_post
+    @post ||= Post.find(params[:id])
+  end
+
+  #Limit params that can be submitted
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
---
+++ db/migrate/20191130213223_create_comment_table.rb
@@ -0,0 +1,10 @@
+class CreateCommentTable < ActiveRecord::Migration[5.1]
+  def change
+    create_table :comments do |t|
+      t.string :text
+      t.references :post, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
